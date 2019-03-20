8f24f97bd5688f29b697cf3e48b38199a07955db
josephdouce <josephdouce@gmail.com>
2019-03-20 19:19:11 +0000

    reverted to npm from yarn, win compatability

--- gui/electron/index.js
+++ gui/electron/index.js
@@ -1,36 +1,22 @@
-const common = require("./common");
-const path = require('path');
+const common = require('./common')
+const path = require('path')
 
-// The profile names should match the ones in ~/.aws/credentials
-let testCredentialsFile = `
-[default]
-aws_access_key_id=testaccesskeyid
-aws_secret_access_key=testaccesskeyid
-[cloudrig]
-aws_access_key_id=testaccessk2eyid3
-aws_secret_access_key=testacces2skeyid3
-`;
+common.registerCMDHook("getCredentials", function(cloudrig, event, op, data, flags) {
+	event.returnValue = cloudrig.getCredentials().toString()
+}, true)
 
-common.registerCMDHook("getCredentials", function (event, op, data) {
-  event.returnValue = testCredentialsFile;
-});
+common.registerCMDHook("saveCredentialsFile", function(cloudrig, event, op, data, flags) {
+	cloudrig.saveCredentialsFile(data);
+	event.returnValue = true;
+}, true)
 
-common.registerCMDHook("saveCredentialsFile", function (event, op, data) {
-  testCredentialsFile = data;
-  event.returnValue = true;
-});
+// TODO: Remove
+common.registerCMDHook("updateFail", function(event, op, data) {
+	event.sender.send('updateCheck', false)
+})
 
-common.registerCMDHook("updateFail", function (event, op, data) {
-  event.sender.send("updateCheck", false);
-});
-
-common.init(
-  {
-    pathname: path.join(__dirname, 'dev.html'),
-    protocol: 'file:',
-    slashes: true
-  },
-  function (win) {
-
-  }
-);
+common.init({
+	pathname: path.join(__dirname, 'build/index.html'),
+	protocol: 'file:',
+	slashes: true
+})
